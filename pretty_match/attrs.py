f3922a4b0ff4db471c753b0c338b60b3ea740297
Skonik <s.konik.job@gmail.com>
2022-07-16 09:11:38 +0300

    feature(attrs): add `FirstNotAttr`

--- pretty_match/attrs.py
+++ pretty_match/attrs.py
@@ -12,7 +12,27 @@ class AttrIsNone:
         return other.name == self.name
 
 
+class AttrIsNot:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return f"<FirstNotAttr: {self.name}>"
+
+    def __repr__(self):
+        return f"<FirstNotAttr: {self.name}>"
+
+    def __eq__(self, other):
+        return other.name == self.name
+
+
 def FirstNoneAttr(*attr_names, instance):
     for attr_name in attr_names:
         if getattr(instance, attr_name) is None:
+            return AttrIsNone(attr_name)
+
+
+def FirstNotAttr(*attr_names, instance):
+    for attr_name in attr_names:
+        if not getattr(instance, attr_name):
             return AttrIsNone(attr_name)
